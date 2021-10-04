bf1565378cb4212f832f03c534151088f5fc6ff6
Markus Kaihola <makeri89@hotmail.com>
2021-10-04 22:14:10 +0300

    added some error handling to reservation page

--- static/script.js
+++ static/script.js
@@ -1,22 +1,23 @@
-const makeReservationDayVisible = () => {
-  const all = document.querySelectorAll(".reservation_date_picker");
-  for (let i = 0; i < all.length; i++) {
-    all[i].style.display = "none";
-  }
-  const datePicker = document.getElementById("date").value;
-  const date = new Date(datePicker).getDay();
-  const days = {
-    0: ".sun",
-    1: ".mon",
-    2: ".tue",
-    3: ".wed",
-    4: ".thu",
-    5: ".fri",
-    6: ".sat",
-  };
-  const timeselector = document.querySelector(days[date]);
-  timeselector.style.display = "block";
-};
+const days = {
+  0: '.sun',
+  1: '.mon',
+  2: '.tue',
+  3: '.wed',
+  4: '.thu',
+  5: '.fri',
+  6: '.sat',
+}
 
-const response = new Response();
-console.log(response.status);
+const makeReservationDayVisible = () => {
+  const notAvailable = document.querySelector('#daynotavailable')
+  notAvailable.style.display = 'none'
+  const all = document.querySelectorAll('.reservation_date_picker')
+  all.forEach(d => (d.style.display = 'none'))
+  const datePicker = document.getElementById('date').value
+  const date = new Date(datePicker).getDay()
+  const timeselector = document.querySelector(days[date])
+  timeselector.style.display = 'block'
+  if (timeselector.value === '') {
+    notAvailable.style.display = 'block'
+  }
+}
